5ef452fa7c12f4ed2d99cd53faf56b13c7a6458b
tondat05 <161530895+tondat05@users.noreply.github.com>
2024-05-01 15:36:57 +0700

    hoàn chỉnh hơn ne

---
+++ SEDictionary - Copy/src/main/java/com/example/sedictionary/mainSence.java
@@ -0,0 +1,71 @@
+package com.example.sedictionary;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class mainSence extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(mainSence.class.getResource("manHinhChinh.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("SE team");
+        stage.setScene(scene);
+        stage.show();
+
+        docFileAnh();
+        docFileViet();
+//        docFileThemTu();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    public static Map<String, String> EngData = new HashMap<>();
+    public static Map<String, String> VieData = new HashMap<>();
+    public static Map<String, String> listThem = new HashMap<>();
+    public static Map<String, String> allWord = new HashMap<>();
+    public void docFileAnh() throws IOException {
+        FileReader doc = new FileReader("data/E_V.txt");
+        BufferedReader br = new BufferedReader(doc);
+        String dong;
+        while ((dong = br.readLine()) != null) {
+            String[] parts = dong.split("<html>");
+            EngData.put(parts[0], parts[1]);
+            allWord.put(parts[0], parts[1]);
+        }
+    }
+    public void docFileViet() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("data/V_E.txt"));
+        String dong;
+        while ((dong = br.readLine()) != null) {
+            String[] parts = dong.split("<html>");
+            VieData.put(parts[0], parts[1]);
+            allWord.put(parts[0], parts[1]);
+        }
+    }
+    public void docFileThemTu() throws IOException {
+        FileReader themBotTu = new FileReader("data/ThemTu.txt");
+        BufferedReader br = new BufferedReader(themBotTu);
+        String dong;
+        while ((dong = br.readLine()) != null){
+            String[] part = dong.split("<html>");
+            listThem.put(part[0], part[1]);
+            allWord.put(part[0], part[1]);
+        }
+    }
+
+
+}
