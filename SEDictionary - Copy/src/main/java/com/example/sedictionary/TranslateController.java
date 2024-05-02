b34fddc5ddf9a73fd4e5cf48fd0a94a401cfbc3a
tondat05 <161530895+tondat05@users.noreply.github.com>
2024-05-02 16:19:06 +0700

    lại

--- SEDictionary - Copy/src/main/java/com/example/sedictionary/TranslateController.java
+++ SEDictionary - Copy/src/main/java/com/example/sedictionary/TranslateController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -20,11 +21,27 @@ public class TranslateController {
     @FXML
     private WebView meaning;
     @FXML
-    private TextField search;
+    private TextArea search;
     @FXML
-    private TextField translationResultTextField;
+    private TextArea translationResultTextField;
+    @FXML
+
 
     public String translate(String target) throws IOException, InterruptedException, ParseException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://google-translate113.p.rapidapi.com/api/v1/translator/text"))
+                .header("content-type", "application/x-www-form-urlencoded")
+                .header("X-RapidAPI-Key", "e54d2f58c1msh485809fba2c35e2p190c06jsnac0dee6572dd")
+                .header("X-RapidAPI-Host", "google-translate113.p.rapidapi.com")
+                .method("POST", HttpRequest.BodyPublishers.ofString("from=en&to=vi&text=" + target))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(response.body());
+        return (String) jsonObject.get("trans");
+    }
+    public String translate2(String target) throws IOException, InterruptedException, ParseException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://google-translate113.p.rapidapi.com/api/v1/translator/text"))
                 .header("content-type", "application/x-www-form-urlencoded")
@@ -41,7 +58,8 @@ public class TranslateController {
 
     public void trans() throws IOException, ParseException, InterruptedException {
         String text = search.getText();
-        String translationResult = translate(text);
+        String translationResult = translate2(text);
+        translationResultTextField.clear();
         translationResultTextField.setText(translationResult);
     }
 
--- SEDictionary - Copy/src/main/java/module-info.java
+++ SEDictionary - Copy/src/main/java/module-info.java
@@ -7,6 +7,9 @@ module com.example.demo1 {
     requires javafx.web;
     requires jsapi;
 
+    requires com.dlsc.formsfx;
+    requires com.almasb.fxgl.all;
+
     opens com.example.sedictionary to javafx.fxml;
     exports com.example.sedictionary;
 }
